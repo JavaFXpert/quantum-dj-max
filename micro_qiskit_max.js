2d06523a88394ce6cb20e8382a0b4c722f0f4e4c
James Weaver <james.l.weaver@gmail.com>
2020-12-20 08:43:42 -0500

    Create Qiskit simulator device

--- micro_qiskit_max.js
+++ micro_qiskit_max.js
@@ -1,71 +1,9 @@
 /**
- * Quantum DJ device user interface
+ * MicroQiskit implementation in Max
  */
 var r2 = 0.70710678118;
 
 this.inlets = 2;
-
-var CircuitNodeTypes = {
-    EMPTY: -1,
-    IDEN: 0,
-    X: 1,
-    Y: 2,
-    Z: 3,
-    S: 4,
-    SDG: 5,
-    T: 6,
-    TDG: 7,
-    H: 8,
-    SWAP: 9,
-    BARRIER: 10,
-    CTRL: 11, // "control" part of multi-qubit gate
-    TRACE: 12, // In the path between a gate part and a "control" or "swap" part
-    MEASURE_Z: 13
-}
-
-var curCircNodeType = CircuitNodeTypes.EMPTY;
-
-var NUM_GRID_ROWS = 8;
-var NUM_GRID_COLS = 5;
-
-var lowMidiPitch = 36;
-var highMidiPitch = NUM_GRID_ROWS * NUM_GRID_COLS + lowMidiPitch - 1;
-post('highMidiPitch: ' + highMidiPitch);
-
-
-var circGrid = [
-    [-1, -1, -1, -1,-1],
-    [-1, -1, -1, -1,-1],
-    [-1, -1, -1, -1,-1],
-    [-1, -1, -1, -1,-1],
-    [-1, -1, -1, -1,-1],
-    [-1, -1, -1, -1,-1],
-    [-1, -1, -1, -1,-1],
-    [-1, -1, -1, -1,-1]
-];
-
-/**
- * Determine how many wires are represented on the circGrid
- */
-function computeNumWires() {
-	var numWires = 1;
-	var foundPopulatedRow = false;
-	var rowIdx = NUM_GRID_ROWS - 1;
-
-	while (!foundPopulatedRow && rowIdx > 0) {
-		for (var colIdx = 0; colIdx < NUM_GRID_COLS; colIdx++) {
-			if (circGrid[rowIdx][colIdx] != CircuitNodeTypes.EMPTY) {
-				numWires = rowIdx + 1;
-				foundPopulatedRow = true;
-			}
-		}
-		rowIdx--;
-	}
-
-	post('numWires: ' + numWires);
-	return numWires;
-}
-
 
 sketch.default2d();
 var val = 0;
@@ -92,121 +30,18 @@ function msg_int(n)
 
 function list(lst) 
 {
-	post('from inlet: ' + inlet);
-	post('arguments.length: ' + arguments.length);
-	post('arguments[0]: ' + arguments[0] + ', arguments[1]: ' + arguments[1]);
-	
-	if (inlet == 0) { 
-		setCircGridGate(arguments);
-	}
-	else if (inlet == 1) {
-		setCurCircNodeType(arguments);
-	}
+	// post('from inlet: ' + inlet);
+	// post('arguments.length: ' + arguments.length);
+	// post('arguments[0]: ' + arguments[0] + ', arguments[1]: ' + arguments[1]);
+
+	// if (inlet == 0) {
+	// 	createQuantumCircuitFromQasm(arguments);
+	// }
+	// else if (inlet == 0) {
+	// 	setCurCircNodeType(arguments);
+	// }
 }
 
-
-/*
-Given an array with midi pitch and velocity, 
-populates the corresponding circuit grid element
-*/
-function setCircGridGate(notePitchVelocity) {
-	//post('notePitchVel: ' + notePitchVel[0]);
-	if (notePitchVelocity.length >= 2) {
-		var pitch = notePitchVelocity[0];
-		var velocity = notePitchVelocity[1];
-	
-		if (pitch >= lowMidiPitch && pitch <= highMidiPitch & velocity > 0) {
-			var gridCol = (highMidiPitch - pitch) % NUM_GRID_COLS;
-			gridCol = NUM_GRID_COLS - gridCol - 1;
-
-      var gridRow = Math.floor((highMidiPitch - pitch) / NUM_GRID_COLS);
-
-      circGrid[gridRow][gridCol] = curCircNodeType;
-			printCircGrid();
-			createQuantumCircuitFromGrid();
-
-		}
-	}
-	else {
-		post('Unexpected notePitchVelocity.length: ' + notePitchVelocity.length);
-	}
-}
-
-
-/*
-Given an array with controller number and value, 
-sets the current circuit node type for when
-a node is placed on the circuit
-*/
-function setCurCircNodeType(controllerNumValue) {
-	post('controllerNumValue: ' + controllerNumValue[0]);
-	if (controllerNumValue.length >= 2) {
-	    var contNum = controllerNumValue[0];
-	    var contVal = controllerNumValue[1];
-	
-	    if (contVal > 0) {
-		    if (contNum == 43) {
-			    curCircNodeType = CircuitNodeTypes.H;
-			}
-			else if (contNum == 42) {
-				curCircNodeType = CircuitNodeTypes.X;
-			}
-			else if (contNum == 41) {
-				curCircNodeType = CircuitNodeTypes.Z;
-			}
-			else if (contNum == 40) {
-				curCircNodeType = CircuitNodeTypes.S;
-			}
-			else if (contNum == 39) {
-				curCircNodeType = CircuitNodeTypes.SDG;
-			}
-			else if (contNum == 38) {
-				curCircNodeType = CircuitNodeTypes.T;
-			}
-			else if (contNum == 37) {
-				curCircNodeType = CircuitNodeTypes.TDG;
-			}
-			else if (contNum == 36) {
-				if (curCircNodeType == CircuitNodeTypes.EMPTY){
-					// User pressed EMPTY key twice, so clear grid
-					resetCircGrid();
-				}
-				curCircNodeType = CircuitNodeTypes.EMPTY;
-			}
-			
-      post('curCircNodeType is now ' + curCircNodeType);
-		}
-	
-
-	}
-	else {
-		post('Unexpected controllerNumValue.length: ' + controllerNumValue.length);
-	}
-}
-
-
-/**
- * Set all elements to EMPTY
- */
-function resetCircGrid() {
-	for (rowIdx = 0; rowIdx < NUM_GRID_ROWS; rowIdx++) {
-		for (colIdx = 0; colIdx < NUM_GRID_COLS; colIdx++) {
-			circGrid[rowIdx][colIdx] = CircuitNodeTypes.EMPTY;
-		}
-	}
-	printCircGrid();
-}
-
-
-function printCircGrid() {
-	post('\n');
-    for (rowIdx = 0; rowIdx < NUM_GRID_ROWS; rowIdx++) {
-        for (colIdx = 0; colIdx < NUM_GRID_COLS; colIdx++) {
-	        post(circGrid[rowIdx][colIdx] + ' ');
-	    }	
-	    post('\n');
-	}	
-}	
 
 
 function draw()
@@ -223,16 +58,10 @@ function draw()
 
 		glcolor(0,0,0,1);
 
-		// for (let wireIdx = 0; wireIdx < NUM_GRID_ROWS; wireIdx++) {
-		// 	moveto(-0.8, 0);
-		// }
-
-		moveto(-0.8, -0.8);
-		lineto(0.8, 0.8);
-		// lineto(10, 10);
-		// lineto(-10, 10);
-		// lineto(-10, -10);
-		text("H");
+		moveto(-1.3, -0.4);
+		//font("plex");
+		fontsize(24);
+		text("µQiskit");
 
 		// draw arc outline
 		// glcolor(0,0,0,1);
@@ -291,49 +120,6 @@ function getvalueof()
 	return val;
 }
 
-// all mouse events are of the form: 
-// onevent <x>, <y>, <button down>, <cmd(PC ctrl)>, <shift>, <capslock>, <option>, <ctrl(PC rbutton)>
-// if you don't care about the additonal modifiers args, you can simply leave them out.
-// one potentially confusing thing is that mouse events are in absolute screen coordinates, 
-// with (0,0) as left top, and (width,height) as right, bottom, while drawing 
-// coordinates are in relative world coordinates, with (0,0) as the center, +1 top, -1 bottom,
-// and x coordinates using a uniform scale based on the y coordinates. to convert between screen 
-// and world coordinates, use sketch.screentoworld(x,y) and sketch.worldtoscreen(x,y,z).
-
-function onclick(x,y,but,cmd,shift,capslock,option,ctrl)
-{
-	// cache mouse position for tracking delta movements
-	last_x = x;
-	last_y = y;
-}
-onclick.local = 1; //private. could be left public to permit "synthetic" events
-
-function ondrag(x,y,but,cmd,shift,capslock,option,ctrl)
-{
-	var f,dy;
-	
-	// calculate delta movements
-	dy = y - last_y;
-	if (shift) { 
-		// fine tune if shift key is down
-		f = val - dy*0.001; 
-	} else {
-		f = val - dy*0.01;
-	}
-	msg_float(f); //set new value with clipping + refresh
-	// cache mouse position for tracking delta movements
-	last_x = x;
-	last_y = y;
-}
-ondrag.local = 1; //private. could be left public to permit "synthetic" events
-
-function ondblclick(x,y,but,cmd,shift,capslock,option,ctrl)
-{
-	last_x = x;
-	last_y = y;
-	msg_float(0); // reset dial?
-}
-ondblclick.local = 1; //private. could be left public to permit "synthetic" events
 
 function forcesize(w,h)
 {
@@ -354,22 +140,98 @@ onresize.local = 1; //private
 
 
 /**
- * Analyze the circuit grid and return a QuantumCircuit
+ * Accept an svsim message
  */
-function createQuantumCircuitFromGrid() {
-	var numCircuitWires = computeNumWires();
-	post('numCircuitWires: ' + numCircuitWires);
-	var qc = new QuantumCircuit(numCircuitWires, numCircuitWires);
+function svsim(qasm) {
+	//post('svsim: ' + arguments.length);
+	//post('qasm: ' + qasm);
+	var qc = createQuantumCircuitFromQasm(qasm);
+	if (qc != null) {
 
-	for (var colIdx = 0; colIdx < NUM_GRID_COLS; colIdx++) {
-		for (var rowIdx = 0; rowIdx < numCircuitWires; rowIdx++) {
-			addGateFromGrid(qc, rowIdx, colIdx);
+		var statevector = simulate(qc, 0, 'statevector');
+		post('\n');
+		post('statevector: ' + statevector);
+	}
+	else {
+		post('Unexpectedly, qc: ' + qc);
+	}
+}
+
+
+/**
+ * Process QASM and return a QuantumCircuit
+ */
+function createQuantumCircuitFromQasm(qasm) {
+	var quantumCircuit = null;
+	var numQuantumWires = 0;
+	var numClassicalWires = 0;
+
+	var qasmArray = qasm.split(';');
+
+	for (var tokenIdx = 0; tokenIdx < qasmArray.length; tokenIdx++) {
+		var instruction = qasmArray[tokenIdx].trim();
+
+		if (instruction.length > 0) {
+			post('\ninstruction: ' + instruction);
+			var keywordArgumentArray = instruction.split(' ');
+			if (keywordArgumentArray.length == 2) {
+				//post('\nkeywordArgumentArray[0]: ' + keywordArgumentArray[0]);
+				//post('\nkeywordArgumentArray[1]: ' + keywordArgumentArray[1]);
+
+				var keyword = keywordArgumentArray[0];
+				var argument = keywordArgumentArray[1];
+
+				var leftBracketPos = argument.indexOf('[');
+				var rightBracketPos = argument.indexOf(']');
+
+				if (leftBracketPos > 0 && rightBracketPos > 0) {
+					var argInt = parseInt(argument.substring(leftBracketPos + 1, rightBracketPos));
+					if (argInt >= 0) {
+						keyword = keyword.trim();
+						if (keyword == 'qreg') {
+							numQuantumWires = argInt;
+						}
+						else if (keyword == 'creg') {
+							numClassicalWires = argInt;
+						}
+						else {
+							// Subsequent instructions should be added to quantum circuit
+							// so make sure it is created
+							if (quantumCircuit == null) {
+								quantumCircuit = new QuantumCircuit(numQuantumWires, numClassicalWires);
+								//post('Creating quantumCircuit, quantumCircuit: ' + quantumCircuit.numQubits);
+							}
+
+							// TODO: Expand list of gates supported
+							if (keyword == 'h') {
+								quantumCircuit.h(argInt);
+							}
+							else if (keyword == 'x') {
+								quantumCircuit.x(argInt);
+							}
+							else if (keyword == 'z') {
+								quantumCircuit.z(argInt);
+							}
+							else if (keyword == 's') {
+								quantumCircuit.s(argInt);
+							}
+							else if (keyword == 'sdg') {
+								quantumCircuit.sdg(argInt);
+							}
+							else if (keyword == 't') {
+								quantumCircuit.t(argInt);
+							}
+							else if (keyword == 'tdg') {
+								quantumCircuit.tdg(argInt);
+							}
+						}
+					}
+				}
+			}
 		}
 	}
 
-	var statevector = simulate(qc, 0, 'statevector');
-	post('\n');
-	post('statevector: ' + statevector);
+	return quantumCircuit;
 }
 
 
