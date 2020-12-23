35765e70af5ea5ebf69fcfa775779d2b22b2938e
James Weaver <james.l.weaver@gmail.com>
2020-12-23 10:05:27 -0500

    Cleanup and delete comments

--- gate_btn.js
+++ gate_btn.js
@@ -3,62 +3,12 @@
  */
 include('common.js');
 
-this.inlets = 2;
-this.outlets = 1;
-
 this.midiNum = 0;
-
-var curCircNodeType = CircuitNodeTypes.EMPTY;
-
-var NUM_GRID_ROWS = 8;
-var NUM_GRID_COLS = 5;
-
-var lowMidiPitch = 36;
-var highMidiPitch = NUM_GRID_ROWS * NUM_GRID_COLS + lowMidiPitch - 1;
-//post('highMidiPitch: ' + highMidiPitch);
-
-
-var circGrid = [
-    [-1, -1, -1, -1,-1],
-    [-1, -1, -1, -1,-1],
-    [-1, -1, -1, -1,-1],
-    [-1, -1, -1, -1,-1],
-    [-1, -1, -1, -1,-1],
-    [-1, -1, -1, -1,-1],
-    [-1, -1, -1, -1,-1],
-    [-1, -1, -1, -1,-1]
-];
-
-/**
- * Determine how many wires are represented on the circGrid
- */
-function computeNumWires() {
-	var numWires = 1;
-	var foundPopulatedRow = false;
-	var rowIdx = NUM_GRID_ROWS - 1;
-
-	while (!foundPopulatedRow && rowIdx > 0) {
-		for (var colIdx = 0; colIdx < NUM_GRID_COLS; colIdx++) {
-			if (circGrid[rowIdx][colIdx] != CircuitNodeTypes.EMPTY) {
-				numWires = rowIdx + 1;
-				foundPopulatedRow = true;
-			}
-		}
-		rowIdx--;
-	}
-
-	//post('numWires: ' + numWires);
-	return numWires;
-}
 
 
 sketch.default2d();
 var val = 0;
 var vbrgb = [1.,1.,1.,1.];
-var vfrgb = [0.5,0.5,0.5,1.];
-var vrgb2 = [0.7,0.7,0.7,1.];
-var last_x = 0;
-var last_y = 0;
 
 // process arguments
 post('processing args');
@@ -68,107 +18,6 @@ if (jsarguments.length > 1) {
 }
 
 draw();
-
-
-function msg_int(n)
-{
-    //post('n: ' + n);    
-}
-
-
-function list(lst) 
-{
-	// post('from inlet: ' + inlet);
-	// post('arguments.length: ' + arguments.length);
-	// post('arguments[0]: ' + arguments[0] + ', arguments[1]: ' + arguments[1]);
-	
-	if (inlet == 0) { 
-		setCircGridGate(arguments);
-	}
-	else if (inlet == 1) {
-		setCurCircNodeType(arguments);
-	}
-}
-
-
-/*
-Given an array with midi pitch and velocity, 
-populates the corresponding circuit grid element
-*/
-function setCircGridGate(notePitchVelocity) {
-	//post('notePitchVel: ' + notePitchVel[0]);
-	if (notePitchVelocity.length >= 2) {
-		var pitch = notePitchVelocity[0];
-		var velocity = notePitchVelocity[1];
-	
-		if (pitch >= lowMidiPitch && pitch <= highMidiPitch & velocity > 0) {
-			var gridCol = (highMidiPitch - pitch) % NUM_GRID_COLS;
-			gridCol = NUM_GRID_COLS - gridCol - 1;
-
-      var gridRow = Math.floor((highMidiPitch - pitch) / NUM_GRID_COLS);
-
-      circGrid[gridRow][gridCol] = curCircNodeType;
-			//printCircGrid();
-			createQasmFromGrid();
-
-		}
-	}
-	else {
-		post('Unexpected notePitchVelocity.length: ' + notePitchVelocity.length);
-	}
-}
-
-
-/*
-Given an array with controller number and value, 
-sets the current circuit node type for when
-a node is placed on the circuit
-*/
-function setCurCircNodeType(controllerNumValue) {
-	//post('controllerNumValue: ' + controllerNumValue[0]);
-	if (controllerNumValue.length >= 2) {
-	    var contNum = controllerNumValue[0];
-	    var contVal = controllerNumValue[1];
-	
-	    if (contVal > 0) {
-		    if (contNum == 43) {
-			    curCircNodeType = CircuitNodeTypes.H;
-			}
-			else if (contNum == 42) {
-				curCircNodeType = CircuitNodeTypes.X;
-			}
-			else if (contNum == 41) {
-				curCircNodeType = CircuitNodeTypes.Z;
-			}
-			else if (contNum == 40) {
-				curCircNodeType = CircuitNodeTypes.S;
-			}
-			else if (contNum == 39) {
-				curCircNodeType = CircuitNodeTypes.SDG;
-			}
-			else if (contNum == 38) {
-				curCircNodeType = CircuitNodeTypes.T;
-			}
-			else if (contNum == 37) {
-				curCircNodeType = CircuitNodeTypes.TDG;
-			}
-			else if (contNum == 36) {
-				if (curCircNodeType == CircuitNodeTypes.EMPTY){
-					// User pressed EMPTY key twice, so clear grid
-					resetCircGrid();
-				}
-				curCircNodeType = CircuitNodeTypes.EMPTY;
-			}
-			
-      post('curCircNodeType is now ' + curCircNodeType);
-		}
-	
-
-	}
-	else {
-		post('Unexpected controllerNumValue.length: ' + controllerNumValue.length);
-	}
-}
 
 
 function draw()
@@ -239,82 +88,46 @@ function draw()
 	}
 }
 
-function bang()
+
+/**
+ * When button is clicked send a message and update its appearance.
+ */
+function onclick(x, y, but, cmd, shift, capslock, option, ctrl)
 {
-	draw();
-	refresh();
-	outlet(0,val);
-}
-
-function msg_float(v)
-{
-	val = Math.min(Math.max(0,v),1);
-	notifyclients();
-	bang();
-}
-
-function set(v)
-{
-	val = Math.min(Math.max(0,v),1);
-	notifyclients();
-	draw();
-	refresh();
-}
-
-
-function setvalueof(v)
-{
-	msg_float(v);
-}
-
-function getvalueof()
-{
-	return val;
-}
-
-// all mouse events are of the form: 
-// onevent <x>, <y>, <button down>, <cmd(PC ctrl)>, <shift>, <capslock>, <option>, <ctrl(PC rbutton)>
-// if you don't care about the additonal modifiers args, you can simply leave them out.
-// one potentially confusing thing is that mouse events are in absolute screen coordinates, 
-// with (0,0) as left top, and (width,height) as right, bottom, while drawing 
-// coordinates are in relative world coordinates, with (0,0) as the center, +1 top, -1 bottom,
-// and x coordinates using a uniform scale based on the y coordinates. to convert between screen 
-// and world coordinates, use sketch.screentoworld(x,y) and sketch.worldtoscreen(x,y,z).
-
-function onclick(x,y,but,cmd,shift,capslock,option,ctrl)
-{
-	// cache mouse position for tracking delta movements
-	last_x = x;
-	last_y = y;
-	post('In onclick');
+	// TODO: Change 'bob' remote message everywhere
 	messnamed('bob', this.midiNum, 127);
 
 	draw();
 	refresh();
 }
-onclick.local = 1; //private. could be left public to permit "synthetic" events
+onclick.local = 1;  //private
 
 
-function ondblclick(x,y,but,cmd,shift,capslock,option,ctrl)
+/**
+ * Force the button to be square
+ *
+ * @param w Proposed width of button
+ * @param h Proposed height of button
+ */
+function forcesize(w, h)
 {
-	last_x = x;
-	last_y = y;
-	msg_float(0); // reset dial?
-}
-ondblclick.local = 1; //private. could be left public to permit "synthetic" events
-
-function forcesize(w,h)
-{
-	if (w!=h) {
+	if (w != h) {
 		h = w;
 		box.size(w,h);
 	}
 }
 forcesize.local = 1; //private
 
-function onresize(w,h)
+
+/**
+ * Attempt to resize the button
+ *
+ * @param w Proposed width of button
+ * @param h Proposed height of button
+ */
+function onresize(w, h)
 {
-	forcesize(w,h);
+	forcesize(w, h);
 	draw();
 	refresh();
 }
